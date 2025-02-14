91c8c3bd69b67691484dc7568ecb3373f2bc17b0
Sohail Raoufi <sohailraufi19@gmail.com>
2025-02-14 22:30:46 +0430

    connect to client, send and receive message

--- http.py
+++ http.py
@@ -1,6 +1,24 @@
-import socket
+from socket import socket, AF_INET, SOCK_STREAM, gethostname
 
 
-server = socket.socket()
+server = socket(AF_INET, SOCK_STREAM, -1)
+server.bind((gethostname(), 8080))
 
-print(server)
+print(f"Listening in {server.getsockname()[0]}:{server.getsockname()[1]}")
+
+while 1:
+    try:
+        server.listen()
+
+        (clientSocket, clientAddress) = server.accept()
+        print(clientSocket)
+        print(clientAddress)
+
+        msg = clientSocket.recv(1024).decode()
+
+        clientSocket.send("Hello from httpy :)".encode())
+        clientSocket.close()
+    except KeyboardInterrupt:
+        server.close()
+
+server.close()
