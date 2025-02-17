fe0149474c73e7f31a7db7396c003256a11249e2
Sohail Raoufi <sohailraufi19@gmail.com>
2025-02-17 21:51:50 +0430

    add clean up and close socket

--- http.py
+++ http.py
@@ -1,24 +1,39 @@
-from socket import socket, AF_INET, SOCK_STREAM, gethostname
+from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR, gethostname
+import sys
+import signal
 
 
 server = socket(AF_INET, SOCK_STREAM, -1)
-server.bind((gethostname(), 8080))
+
+server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
+server.bind((gethostname(), 8001))
 
 print(f"Listening in {server.getsockname()[0]}:{server.getsockname()[1]}")
 
+
+def close_and_cleanup(sig, frame):
+    print("\nShuting down the server.......")
+    server.close()
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, close_and_cleanup)
+
+server.listen()
 while 1:
     try:
-        server.listen()
-
         (clientSocket, clientAddress) = server.accept()
-        print(clientSocket)
         print(clientAddress)
+        msg = clientSocket.recv(2024).decode()
+        clientSocket.send("""HTTP/1.1 200 OK
+Content-Type: text/html
+Content-Length: 30
 
-        msg = clientSocket.recv(1024).decode()
-
-        clientSocket.send("Hello from httpy :)".encode())
+<html><body>Hello World</body></html>
+""".encode())
         clientSocket.close()
-    except KeyboardInterrupt:
-        server.close()
+    except OSError:
+        break
 
 server.close()
